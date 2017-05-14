18e75222791fb48e9874742e8096ae89dd1a609b
uxff <uxff@qq.com>
2017-05-14 17:25:14 +0800

    use a149 code

---
+++ backend/config/main-local.php
@@ -0,0 +1,32 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'isk-vbDHIq_eWMzWCsAaZSVprARZ_YDc',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=moblog',
+            'username' => 'www',
+            'password' => '123x456',
+            'charset' => 'utf8',
+        ],
+        'mc' => [
+            'class' => 'yii\caching\MemCache',
+            'useMemcached' => true,
+            'servers' => [
+                [
+                    'host' => '127.0.0.1',
+                    'port' => 11211,
+                    'weight' => 60,
+                ],
+            ],
+        ],
+    ],
+
+];
+
+
+return $config;
---
+++ common/data/blog.sql
@@ -0,0 +1,164 @@
+﻿/*
+ Navicat Premium Data Transfer
+
+ Source Server         : vagrant
+ Source Server Type    : MySQL
+ Source Server Version : 50540
+ Source Host           : 192.168.10.10
+ Source Database       : blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 50540
+ File Encoding         : utf-8
+
+ Date: 01/23/2015 22:53:02 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `moblog_comments`
+-- ----------------------------
+DROP TABLE IF EXISTS `moblog_comments`;
+CREATE TABLE `moblog_comments` (
+  `coid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '文章id',
+  `created` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `author` varchar(200) NOT NULL COMMENT '作者',
+  `authorId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '作者id',
+  `ownerId` int(10) unsigned NOT NULL DEFAULT '0',
+  `mail` varchar(200) NOT NULL COMMENT '邮箱',
+  `url` varchar(200) NOT NULL COMMENT '文章地址',
+  `ip` varchar(64) NOT NULL COMMENT 'ip地址',
+  `agent` varchar(200) NOT NULL COMMENT '代理',
+  `text` text NOT NULL COMMENT '评论内容',
+  `type` varchar(16) NOT NULL DEFAULT 'comment' COMMENT '评论类型',
+  `status` varchar(16) NOT NULL DEFAULT 'approved' COMMENT '状态',
+  `parent` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父节点',
+  PRIMARY KEY (`coid`),
+  KEY `cid` (`cid`),
+  KEY `created` (`created`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+--  Table structure for `moblog_contents`
+-- ----------------------------
+DROP TABLE IF EXISTS `moblog_contents`;
+CREATE TABLE `moblog_contents` (
+  `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL COMMENT '标题',
+  `slug` varchar(200) NOT NULL COMMENT '缩略名',
+  `created` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '发布日期',
+  `modified` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  `text` text NOT NULL COMMENT '内容',
+  `order` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '页面顺序',
+  `authorId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '作者id',
+  `template` varchar(32) NOT NULL COMMENT '模板',
+  `type` varchar(16) NOT NULL DEFAULT 'post' COMMENT '类型',
+  `status` varchar(16) NOT NULL DEFAULT 'publish' COMMENT '公开度',
+  `password` varchar(32) NOT NULL COMMENT '密码',
+  `commentsNum` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评论数',
+  `allowComment` char(1) NOT NULL DEFAULT '0' COMMENT '允许评论',
+  `allowPing` char(1) NOT NULL DEFAULT '0' COMMENT '允许被引用',
+  `allowFeed` char(1) NOT NULL DEFAULT '0' COMMENT '允许在聚合中出现',
+  `parent` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '所属文章',
+  `viewNum` int(11) NOT NULL COMMENT '阅读数',
+  `viewDoneNum` int(11) NOT NULL COMMENT '看完数',
+  PRIMARY KEY (`cid`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `created` (`created`)
+) ENGINE=MyISAM AUTO_INCREMENT=77 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `moblog_metas`
+-- ----------------------------
+DROP TABLE IF EXISTS `moblog_metas`;
+CREATE TABLE `moblog_metas` (
+  `mid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Mid',
+  `name` varchar(200) NOT NULL COMMENT '名称',
+  `slug` varchar(200) NOT NULL COMMENT '缩略名',
+  `type` varchar(32) NOT NULL COMMENT '类型',
+  `description` varchar(200) NOT NULL COMMENT '描述',
+  `count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '文章数',
+  `order` int(10) unsigned NOT NULL DEFAULT '0',
+  `parent` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  PRIMARY KEY (`mid`),
+  KEY `slug` (`slug`)
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `moblog_migration`
+-- ----------------------------
+DROP TABLE IF EXISTS `moblog_migration`;
+CREATE TABLE `moblog_migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) NOT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `moblog_migration`
+-- ----------------------------
+BEGIN;
+INSERT INTO `moblog_migration` VALUES ('m000000_000000_base', '1414912794'), ('m130524_201442_init', '1414913032');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `moblog_options`
+-- ----------------------------
+DROP TABLE IF EXISTS `moblog_options`;
+CREATE TABLE `moblog_options` (
+  `name` varchar(32) NOT NULL,
+  `user` int(10) unsigned NOT NULL DEFAULT '0',
+  `value` text NOT NULL,
+  PRIMARY KEY (`name`,`user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `moblog_options`
+-- ----------------------------
+BEGIN;
+INSERT INTO `moblog_options` VALUES ('theme', '0', 'default'), ('theme:default', '0', 'a:2:{s:7:\"logoUrl\";N;s:12:\"sidebarBlock\";a:5:{i:0;s:15:\"ShowRecentPosts\";i:1;s:18:\"ShowRecentComments\";i:2;s:12:\"ShowCategory\";i:3;s:11:\"ShowArchive\";i:4;s:9:\"ShowOther\";}}'), ('timezone', '0', '28800'), ('lang', '0', 'zh_CN'), ('charset', '0', 'UTF-8'), ('contentType', '0', 'text/html'), ('gzip', '0', '0'), ('generator', '0', 'Typecho 1.0/14.10.10'), ('title', '0', '墨迹凡'), ('description', '0', '记录一些点点滴滴'), ('keywords', '0', '墨迹凡,moblog,yii2,php,mysql,linux'), ('rewrite', '0', '0'), ('frontPage', '0', 'recent'), ('frontArchive', '0', '0'), ('commentsRequireMail', '0', '1'), ('commentsWhitelist', '0', '0'), ('commentsRequireURL', '0', '0'), ('commentsRequireModeration', '0', '0'), ('plugins', '0', 'a:2:{s:9:\"activated\";a:0:{}s:7:\"handles\";a:0:{}}'), ('commentDateFormat', '0', 'F jS, Y \\a\\t h:i a'), ('siteUrl', '0', 'http://typecho.m.com'), ('defaultCategory', '0', '1'), ('allowRegister', '0', '0'), ('defaultAllowComment', '0', '1'), ('defaultAllowPing', '0', '1'), ('defaultAllowFeed', '0', '1'), ('pageSize', '0', '5'), ('postsListSize', '0', '10'), ('commentsListSize', '0', '10'), ('commentsHTMLTagAllowed', '0', null), ('postDateFormat', '0', 'Y-m-d'), ('feedFullText', '0', '1'), ('editorSize', '0', '350'), ('autoSave', '0', '0'), ('markdown', '0', '1'), ('commentsMaxNestingLevels', '0', '5'), ('commentsPostTimeout', '0', '2592000'), ('commentsUrlNofollow', '0', '1'), ('commentsShowUrl', '0', '1'), ('commentsMarkdown', '0', '0'), ('commentsPageBreak', '0', '0'), ('commentsThreaded', '0', '1'), ('commentsPageSize', '0', '20'), ('commentsPageDisplay', '0', 'last'), ('commentsOrder', '0', 'ASC'), ('commentsCheckReferer', '0', '1'), ('commentsAutoClose', '0', '0'), ('commentsPostIntervalEnable', '0', '1'), ('commentsPostInterval', '0', '60'), ('commentsShowCommentOnly', '0', '0'), ('commentsAvatar', '0', '1'), ('commentsAvatarRating', '0', 'G'), ('commentsAntiSpam', '0', '1'), ('routingTable', '0', 'a:26:{i:0;a:25:{s:5:\"index\";a:6:{s:3:\"url\";s:1:\"/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:8:\"|^[/]?$|\";s:6:\"format\";s:1:\"/\";s:6:\"params\";a:0:{}}s:7:\"archive\";a:6:{s:3:\"url\";s:6:\"/blog/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:13:\"|^/blog[/]?$|\";s:6:\"format\";s:6:\"/blog/\";s:6:\"params\";a:0:{}}s:2:\"do\";a:6:{s:3:\"url\";s:22:\"/action/[action:alpha]\";s:6:\"widget\";s:9:\"Widget_Do\";s:6:\"action\";s:6:\"action\";s:4:\"regx\";s:32:\"|^/action/([_0-9a-zA-Z-]+)[/]?$|\";s:6:\"format\";s:10:\"/action/%s\";s:6:\"params\";a:1:{i:0;s:6:\"action\";}}s:4:\"post\";a:6:{s:3:\"url\";s:24:\"/archives/[cid:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:26:\"|^/archives/([0-9]+)[/]?$|\";s:6:\"format\";s:13:\"/archives/%s/\";s:6:\"params\";a:1:{i:0;s:3:\"cid\";}}s:10:\"attachment\";a:6:{s:3:\"url\";s:26:\"/attachment/[cid:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:28:\"|^/attachment/([0-9]+)[/]?$|\";s:6:\"format\";s:15:\"/attachment/%s/\";s:6:\"params\";a:1:{i:0;s:3:\"cid\";}}s:8:\"category\";a:6:{s:3:\"url\";s:17:\"/category/[slug]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:25:\"|^/category/([^/]+)[/]?$|\";s:6:\"format\";s:13:\"/category/%s/\";s:6:\"params\";a:1:{i:0;s:4:\"slug\";}}s:3:\"tag\";a:6:{s:3:\"url\";s:12:\"/tag/[slug]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:20:\"|^/tag/([^/]+)[/]?$|\";s:6:\"format\";s:8:\"/tag/%s/\";s:6:\"params\";a:1:{i:0;s:4:\"slug\";}}s:6:\"author\";a:6:{s:3:\"url\";s:22:\"/author/[uid:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:24:\"|^/author/([0-9]+)[/]?$|\";s:6:\"format\";s:11:\"/author/%s/\";s:6:\"params\";a:1:{i:0;s:3:\"uid\";}}s:6:\"search\";a:6:{s:3:\"url\";s:19:\"/search/[keywords]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:23:\"|^/search/([^/]+)[/]?$|\";s:6:\"format\";s:11:\"/search/%s/\";s:6:\"params\";a:1:{i:0;s:8:\"keywords\";}}s:10:\"index_page\";a:6:{s:3:\"url\";s:21:\"/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:22:\"|^/page/([0-9]+)[/]?$|\";s:6:\"format\";s:9:\"/page/%s/\";s:6:\"params\";a:1:{i:0;s:4:\"page\";}}s:12:\"archive_page\";a:6:{s:3:\"url\";s:26:\"/blog/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:27:\"|^/blog/page/([0-9]+)[/]?$|\";s:6:\"format\";s:14:\"/blog/page/%s/\";s:6:\"params\";a:1:{i:0;s:4:\"page\";}}s:13:\"category_page\";a:6:{s:3:\"url\";s:32:\"/category/[slug]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:34:\"|^/category/([^/]+)/([0-9]+)[/]?$|\";s:6:\"format\";s:16:\"/category/%s/%s/\";s:6:\"params\";a:2:{i:0;s:4:\"slug\";i:1;s:4:\"page\";}}s:8:\"tag_page\";a:6:{s:3:\"url\";s:27:\"/tag/[slug]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:29:\"|^/tag/([^/]+)/([0-9]+)[/]?$|\";s:6:\"format\";s:11:\"/tag/%s/%s/\";s:6:\"params\";a:2:{i:0;s:4:\"slug\";i:1;s:4:\"page\";}}s:11:\"author_page\";a:6:{s:3:\"url\";s:37:\"/author/[uid:digital]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:33:\"|^/author/([0-9]+)/([0-9]+)[/]?$|\";s:6:\"format\";s:14:\"/author/%s/%s/\";s:6:\"params\";a:2:{i:0;s:3:\"uid\";i:1;s:4:\"page\";}}s:11:\"search_page\";a:6:{s:3:\"url\";s:34:\"/search/[keywords]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:32:\"|^/search/([^/]+)/([0-9]+)[/]?$|\";s:6:\"format\";s:14:\"/search/%s/%s/\";s:6:\"params\";a:2:{i:0;s:8:\"keywords\";i:1;s:4:\"page\";}}s:12:\"archive_year\";a:6:{s:3:\"url\";s:18:\"/[year:digital:4]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:19:\"|^/([0-9]{4})[/]?$|\";s:6:\"format\";s:4:\"/%s/\";s:6:\"params\";a:1:{i:0;s:4:\"year\";}}s:13:\"archive_month\";a:6:{s:3:\"url\";s:36:\"/[year:digital:4]/[month:digital:2]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:30:\"|^/([0-9]{4})/([0-9]{2})[/]?$|\";s:6:\"format\";s:7:\"/%s/%s/\";s:6:\"params\";a:2:{i:0;s:4:\"year\";i:1;s:5:\"month\";}}s:11:\"archive_day\";a:6:{s:3:\"url\";s:52:\"/[year:digital:4]/[month:digital:2]/[day:digital:2]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:41:\"|^/([0-9]{4})/([0-9]{2})/([0-9]{2})[/]?$|\";s:6:\"format\";s:10:\"/%s/%s/%s/\";s:6:\"params\";a:3:{i:0;s:4:\"year\";i:1;s:5:\"month\";i:2;s:3:\"day\";}}s:17:\"archive_year_page\";a:6:{s:3:\"url\";s:38:\"/[year:digital:4]/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:33:\"|^/([0-9]{4})/page/([0-9]+)[/]?$|\";s:6:\"format\";s:12:\"/%s/page/%s/\";s:6:\"params\";a:2:{i:0;s:4:\"year\";i:1;s:4:\"page\";}}s:18:\"archive_month_page\";a:6:{s:3:\"url\";s:56:\"/[year:digital:4]/[month:digital:2]/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:44:\"|^/([0-9]{4})/([0-9]{2})/page/([0-9]+)[/]?$|\";s:6:\"format\";s:15:\"/%s/%s/page/%s/\";s:6:\"params\";a:3:{i:0;s:4:\"year\";i:1;s:5:\"month\";i:2;s:4:\"page\";}}s:16:\"archive_day_page\";a:6:{s:3:\"url\";s:72:\"/[year:digital:4]/[month:digital:2]/[day:digital:2]/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:55:\"|^/([0-9]{4})/([0-9]{2})/([0-9]{2})/page/([0-9]+)[/]?$|\";s:6:\"format\";s:18:\"/%s/%s/%s/page/%s/\";s:6:\"params\";a:4:{i:0;s:4:\"year\";i:1;s:5:\"month\";i:2;s:3:\"day\";i:3;s:4:\"page\";}}s:12:\"comment_page\";a:6:{s:3:\"url\";s:53:\"[permalink:string]/comment-page-[commentPage:digital]\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:36:\"|^(.+)/comment\\-page\\-([0-9]+)[/]?$|\";s:6:\"format\";s:18:\"%s/comment-page-%s\";s:6:\"params\";a:2:{i:0;s:9:\"permalink\";i:1;s:11:\"commentPage\";}}s:4:\"feed\";a:6:{s:3:\"url\";s:20:\"/feed[feed:string:0]\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:4:\"feed\";s:4:\"regx\";s:17:\"|^/feed(.*)[/]?$|\";s:6:\"format\";s:7:\"/feed%s\";s:6:\"params\";a:1:{i:0;s:4:\"feed\";}}s:8:\"feedback\";a:6:{s:3:\"url\";s:31:\"[permalink:string]/[type:alpha]\";s:6:\"widget\";s:15:\"Widget_Feedback\";s:6:\"action\";s:6:\"action\";s:4:\"regx\";s:29:\"|^(.+)/([_0-9a-zA-Z-]+)[/]?$|\";s:6:\"format\";s:5:\"%s/%s\";s:6:\"params\";a:2:{i:0;s:9:\"permalink\";i:1;s:4:\"type\";}}s:4:\"page\";a:6:{s:3:\"url\";s:12:\"/[slug].html\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";s:4:\"regx\";s:22:\"|^/([^/]+)\\.html[/]?$|\";s:6:\"format\";s:8:\"/%s.html\";s:6:\"params\";a:1:{i:0;s:4:\"slug\";}}}s:5:\"index\";a:3:{s:3:\"url\";s:1:\"/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:7:\"archive\";a:3:{s:3:\"url\";s:6:\"/blog/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:2:\"do\";a:3:{s:3:\"url\";s:22:\"/action/[action:alpha]\";s:6:\"widget\";s:9:\"Widget_Do\";s:6:\"action\";s:6:\"action\";}s:4:\"post\";a:3:{s:3:\"url\";s:24:\"/archives/[cid:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:10:\"attachment\";a:3:{s:3:\"url\";s:26:\"/attachment/[cid:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:8:\"category\";a:3:{s:3:\"url\";s:17:\"/category/[slug]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:3:\"tag\";a:3:{s:3:\"url\";s:12:\"/tag/[slug]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:6:\"author\";a:3:{s:3:\"url\";s:22:\"/author/[uid:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:6:\"search\";a:3:{s:3:\"url\";s:19:\"/search/[keywords]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:10:\"index_page\";a:3:{s:3:\"url\";s:21:\"/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:12:\"archive_page\";a:3:{s:3:\"url\";s:26:\"/blog/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:13:\"category_page\";a:3:{s:3:\"url\";s:32:\"/category/[slug]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:8:\"tag_page\";a:3:{s:3:\"url\";s:27:\"/tag/[slug]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:11:\"author_page\";a:3:{s:3:\"url\";s:37:\"/author/[uid:digital]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:11:\"search_page\";a:3:{s:3:\"url\";s:34:\"/search/[keywords]/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:12:\"archive_year\";a:3:{s:3:\"url\";s:18:\"/[year:digital:4]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:13:\"archive_month\";a:3:{s:3:\"url\";s:36:\"/[year:digital:4]/[month:digital:2]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:11:\"archive_day\";a:3:{s:3:\"url\";s:52:\"/[year:digital:4]/[month:digital:2]/[day:digital:2]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:17:\"archive_year_page\";a:3:{s:3:\"url\";s:38:\"/[year:digital:4]/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:18:\"archive_month_page\";a:3:{s:3:\"url\";s:56:\"/[year:digital:4]/[month:digital:2]/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:16:\"archive_day_page\";a:3:{s:3:\"url\";s:72:\"/[year:digital:4]/[month:digital:2]/[day:digital:2]/page/[page:digital]/\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:12:\"comment_page\";a:3:{s:3:\"url\";s:53:\"[permalink:string]/comment-page-[commentPage:digital]\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}s:4:\"feed\";a:3:{s:3:\"url\";s:20:\"/feed[feed:string:0]\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:4:\"feed\";}s:8:\"feedback\";a:3:{s:3:\"url\";s:31:\"[permalink:string]/[type:alpha]\";s:6:\"widget\";s:15:\"Widget_Feedback\";s:6:\"action\";s:6:\"action\";}s:4:\"page\";a:3:{s:3:\"url\";s:12:\"/[slug].html\";s:6:\"widget\";s:14:\"Widget_Archive\";s:6:\"action\";s:6:\"render\";}}'), ('actionTable', '0', 'a:0:{}'), ('panelTable', '0', 'a:0:{}'), ('attachmentTypes', '0', '@image@'), ('secret', '0', 'zF8waDMhjA#AExMU7wSXQbP9mbwv16rh');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `moblog_relationships`
+-- ----------------------------
+DROP TABLE IF EXISTS `moblog_relationships`;
+CREATE TABLE `moblog_relationships` (
+  `cid` int(10) unsigned NOT NULL,
+  `mid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cid`,`mid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `moblog_users`
+-- ----------------------------
+DROP TABLE IF EXISTS `moblog_users`;
+CREATE TABLE `moblog_users` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Uid',
+  `name` varchar(32) NOT NULL COMMENT '用户名',
+  `password` varchar(64) NOT NULL COMMENT '密码',
+  `mail` varchar(200) NOT NULL COMMENT '邮箱',
+  `url` varchar(200) NOT NULL COMMENT '个人主页',
+  `screenName` varchar(32) NOT NULL COMMENT '昵称',
+  `created` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `activated` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '活跃时间',
+  `logged` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '登录时间',
+  `group` varchar(16) NOT NULL DEFAULT 'visitor' COMMENT '用户组',
+  `authCode` varchar(64) NOT NULL COMMENT 'Auth Code',
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `mail` (`mail`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `moblog_users`
+-- ----------------------------
+BEGIN;
+INSERT INTO `moblog_users` VALUES ('1', 'admin', '$2y$13$j..g2Qy6U/uyhdb9vPOIA.ZepAuKGLQUMw.rzaZ3rIT40fXzAZvgq', 'webmaster@yourdomain.com', 'http://www.typecho.org', 'admin', '1414766632', '1415195935', '1414847843', 'administrator', 'be85fbd41970b580a633cd679b82ae38');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
