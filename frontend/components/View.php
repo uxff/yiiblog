b7cbe5e4af9c44cb7e614cc5a4f83a9740ff276a
uxff <uxff@qq.com>
2017-05-14 17:08:16 +0800

    re init

--- frontend/components/View.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @author: mojifan [<https://github.com/mojifan>]
- */
-namespace frontend\components;
-class View extends \yii\web\View{
-    public $keywords;
-    public $description;
-}
